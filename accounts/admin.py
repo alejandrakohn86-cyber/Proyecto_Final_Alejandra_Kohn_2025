f0273399e00d9fe6611cd76a314178ae1979ba84
alejandrakohn86-cyber <alejandra.kohn86@gmail.com>
2025-10-18 23:12:48 -0300

    Revert "Initial commit"
    This reverts commit 1e9ffae54b46f8139e5d4bffb45d365b01b7d44b.

--- accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'fecha_nacimiento']
-    search_fields = ['user__username', 'user__email']
